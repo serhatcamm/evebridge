8d2b80825b84eeae17bf1332508c8d8d09ec77b0
serhatcamm <serhatcamm@users.noreply.github.com>
2026-08-24 23:33:01 +0300

    Online Image Store: add 47 labhub.eu.org images (95 total, 29 vendors)
    Probed legacy.labhub.eu.org/1:/addons/qemu/ live and verified all
    paths. New entries: FortiGate 7.4.x, PAN-OS 11.x, vIOS 15.9M5,
    vIOS-L2 2023, CSR1000v 17.06.x, Cat8000v 17.10-17.12, ISE 3.2/3.3,
    FTD 7.2, FMC 7.0, NX-OSv 9.3.3, Ubuntu 22.04/24.04, Debian 11,
    Kali 2024, CentOS 9, MikroTik 7.12-7.15, Server 2016/2022,
    Windows 10 22H2/11 22H2, vEOS 4.29-4.31, Aruba CX 10.09/10.10,
    ClearPass 6.11, BIGIP 16.1/17.1, vSRX 21.4/22.4, vMX 21.2/22.3

--- image_store.py
+++ image_store.py
@@ -140,6 +140,104 @@ _add("Windows", "Server 2019 R2 x64", "winserver-S2019-R2-x64-rev3.tgz",
 _add("Windows", "Windows 10 x64 21H1", "win-10-x64-21H1v1.tgz",
      "https://legacy.labhub.eu.org/1:/addons/qemu/Windows/win-10-x64-21H1v1.tgz", "win", 4096)
 
+# ---------------- LabHub.eu.org (verified live) ----------------
+_LH = "https://legacy.labhub.eu.org/1:/addons/qemu"
+
+_add("Fortinet", "FortiGate 7.4.4", "fortinet-FGT-v7.4.4-build2571.tgz",
+     f"{_LH}/Fortinet/fortinet-FGT-v7.4.4-build2571.tgz", "fortinet", 2048)
+_add("Fortinet", "FortiGate 7.4.3", "fortinet-FGT-v7.4.3-build2532.tgz",
+     f"{_LH}/Fortinet/fortinet-FGT-v7.4.3-build2532.tgz", "fortinet", 2048)
+_add("Fortinet", "FortiGate 7.2.5", "fortinet-FGT-v7.2.5-build1396.tgz",
+     f"{_LH}/Fortinet/fortinet-FGT-v7.2.5-build1396.tgz", "fortinet", 2048)
+_add("Fortinet", "FortiManager 7.4.1", "fortinet-FMG-v7.4.1-build2512.tgz",
+     f"{_LH}/Fortinet/fortinet-FMG-v7.4.1-build2512.tgz", "fortinet", 4096)
+_add("Palo Alto", "PAN-OS 11.1.2", "paloalto-11.1.2.tar.gz",
+     f"{_LH}/Palo%20Alto/paloalto-11.1.2.tar.gz", "paloalto", 4096)
+_add("Palo Alto", "PAN-OS 11.0.3", "paloalto-11.0.3.tar.gz",
+     f"{_LH}/Palo%20Alto/paloalto-11.0.3.tar.gz", "paloalto", 4096)
+_add("Palo Alto", "PAN-OS 10.2.9", "paloalto-10.2.9.tar.gz",
+     f"{_LH}/Palo%20Alto/paloalto-10.2.9.tar.gz", "paloalto", 4096)
+_add("Palo Alto", "PAN-OS 11.1.0", "paloalto-11.1.0.tar.gz",
+     f"{_LH}/Palo%20Alto/paloalto-11.1.0.tar.gz", "paloalto", 4096)
+_add("Palo Alto", "Panorama 10.2.3", "panorama-10.2.3.tar.gz",
+     f"{_LH}/Palo%20Alto/panorama-10.2.3.tar.gz", "panorama", 4096)
+_add("Cisco vIOS", "vIOS 15.9(3)M5", "vios-adventerprisek9-m.SPA.159-3.M5.tgz",
+     f"{_LH}/Cisco%20IOSv/vios-adventerprisek9-m.SPA.159-3.M5.tgz", "vios", 1024)
+_add("Cisco vIOS-L2", "vIOS-L2 2023-07", "viosl2-adventerprisek9-m.SSA.20230712.tgz",
+     f"{_LH}/Cisco%20IOSvL2/viosl2-adventerprisek9-m.SSA.20230712.tgz", "viosl2", 1024)
+_add("Cisco CSR1000v", "CSR1000v 17.06.05", "csr1000vng-universalk9.17.06.05-serial.tgz",
+     f"{_LH}/Cisco%20CSR1000v/csr1000vng-universalk9.17.06.05-serial.tgz", "csr1000vng", 4096)
+_add("Cisco CSR1000v", "CSR1000v 17.06.03a", "csr1000vng-universalk9.17.06.03a-serial.tgz",
+     f"{_LH}/Cisco%20CSR1000v/csr1000vng-universalk9.17.06.03a-serial.tgz", "csr1000vng", 4096)
+_add("Cisco Cat8000v", "Cat8000v 17.12.01a", "c8000v-17.12.01a.tgz",
+     f"{_LH}/Cisco%20C8000v/c8000v-17.12.01a.tgz", "c8000v", 4096)
+_add("Cisco Cat8000v", "Cat8000v 17.11.01a", "c8000v-17.11.01a.tgz",
+     f"{_LH}/Cisco%20C8000v/c8000v-17.11.01a.tgz", "c8000v", 4096)
+_add("Cisco Cat8000v", "Cat8000v 17.10.01a", "c8000v-17.10.01a.tgz",
+     f"{_LH}/Cisco%20C8000v/c8000v-17.10.01a.tgz", "c8000v", 4096)
+_add("Cisco ISE", "ISE 3.2.0.542", "ise-3.2.0.542.tgz",
+     f"{_LH}/Cisco%20ISE/ise-3.2.0.542.tgz", "ise", 16384)
+_add("Cisco ISE", "ISE 3.3.0.531", "ise-3.3.0.531.tgz",
+     f"{_LH}/Cisco%20ISE/ise-3.3.0.531.tgz", "ise", 16384)
+_add("Cisco FirePOWER", "FTD 7.2.3-64", "firepower7-FTD-7.2.3-64.tgz",
+     f"{_LH}/Cisco%20FirePOWER/firepower7-FTD-7.2.3-64.tgz", "firepower7", 8192)
+_add("Cisco FirePOWER", "FMC 7.0.1-84", "firepower6-FMC-7.0.1-84.tgz",
+     f"{_LH}/Cisco%20FirePOWER/firepower6-FMC-7.0.1-84.tgz", "firepower6", 16384)
+_add("Cisco Nexus", "NX-OS 9000v 9.3.3", "nexus-9000v-9.3.3.tgz",
+     f"{_LH}/Cisco%20NX-OS%20Titanium/nexus-9000v-9.3.3.tgz", "titanium", 4096)
+_add("Linux", "Ubuntu Server 22.04", "linux-ubuntu-server-22.04.tgz",
+     f"{_LH}/Linux/linux-ubuntu-server-22.04.tgz", "linux", 1024)
+_add("Linux", "Ubuntu Server 24.04", "linux-ubuntu-server-24.04.tgz",
+     f"{_LH}/Linux/linux-ubuntu-server-24.04.tgz", "linux", 1024)
+_add("Linux", "Ubuntu Desktop 22.04", "linux-ubuntu-desktop-22.04.tgz",
+     f"{_LH}/Linux/linux-ubuntu-desktop-22.04.tgz", "linux", 2048)
+_add("Linux", "Debian 11", "linux-debian-11.tgz",
+     f"{_LH}/Linux/linux-debian-11.tgz", "linux", 1024)
+_add("Linux", "Kali 2024.1", "linux-kali-2024.1.tgz",
+     f"{_LH}/Linux/linux-kali-2024.1.tgz", "linux", 2048)
+_add("Linux", "CentOS 9", "linux-centos-9.tgz",
+     f"{_LH}/Linux/linux-centos-9.tgz", "linux", 1024)
+_add("MikroTik", "RouterOS 7.15.3", "mikrotik-7.15.3.tgz",
+     f"{_LH}/Mikrotik/mikrotik-7.15.3.tgz", "mikrotik", 512)
+_add("MikroTik", "RouterOS 7.14.3", "mikrotik-7.14.3.tgz",
+     f"{_LH}/Mikrotik/mikrotik-7.14.3.tgz", "mikrotik", 512)
+_add("MikroTik", "RouterOS 7.13.5", "mikrotik-7.13.5.tgz",
+     f"{_LH}/Mikrotik/mikrotik-7.13.5.tgz", "mikrotik", 512)
+_add("MikroTik", "RouterOS 7.12.1", "mikrotik-7.12.1.tgz",
+     f"{_LH}/Mikrotik/mikrotik-7.12.1.tgz", "mikrotik", 512)
+_add("Windows", "Server 2022 x64", "winserver-S2022-x64.tgz",
+     f"{_LH}/Windows/winserver-S2022-x64.tgz", "winserver", 4096)
+_add("Windows", "Server 2016 x64", "winserver-S2016-x64.tgz",
+     f"{_LH}/Windows/winserver-S2016-x64.tgz", "winserver", 4096)
+_add("Windows", "Windows 11 22H2", "win-11-x64-22H2v2.tgz",
+     f"{_LH}/Windows/win-11-x64-22H2v2.tgz", "win", 4096)
+_add("Windows", "Windows 10 22H2", "win-10-x64-22H2v1.tgz",
+     f"{_LH}/Windows/win-10-x64-22H2v1.tgz", "win", 4096)
+_add("Arista", "vEOS 4.31.1F", "veos-4.31.1F.tgz",
+     f"{_LH}/Arista/veos-4.31.1F.tgz", "veos", 2048)
+_add("Arista", "vEOS 4.30.1F", "veos-4.30.1F.tgz",
+     f"{_LH}/Arista/veos-4.30.1F.tgz", "veos", 2048)
+_add("Arista", "vEOS 4.29.1F", "veos-4.29.1F.tgz",
+     f"{_LH}/Arista/veos-4.29.1F.tgz", "veos", 2048)
+_add("Aruba", "Aruba CX 10.10", "arubacx-10.10.tgz",
+     f"{_LH}/Aruba%20CX/arubacx-10.10.tgz", "arubacx", 4096)
+_add("Aruba", "Aruba CX 10.09", "arubacx-10.09.tgz",
+     f"{_LH}/Aruba%20CX/arubacx-10.09.tgz", "arubacx", 4096)
+_add("Aruba", "ClearPass 6.11", "clearpass-6.11.0.tgz",
+     f"{_LH}/Aruba%20ClearPass/clearpass-6.11.0.tgz", "clearpass", 8192)
+_add("F5", "BIGIP 17.1.0", "bigip-17.1.0-0.0.3.tgz",
+     f"{_LH}/F5%20BIGIP/bigip-17.1.0-0.0.3.tgz", "bigip", 8192)
+_add("F5", "BIGIP 16.1.3", "bigip-16.1.3-0.0.28.tgz",
+     f"{_LH}/F5%20BIGIP/bigip-16.1.3-0.0.28.tgz", "bigip", 8192)
+_add("Juniper", "vSRX 21.4R1.12", "vsrxng-21.4R1.12.tgz",
+     f"{_LH}/Juniper/vsrxng-21.4R1.12.tgz", "vsrxng", 4096)
+_add("Juniper", "vSRX 22.4R2.8", "vsrxng-22.4R2.8.tgz",
+     f"{_LH}/Juniper/vsrxng-22.4R2.8.tgz", "vsrxng", 4096)
+_add("Juniper", "vMX 21.2R3.11", "vmx-21.2R3.11.tgz",
+     f"{_LH}/Juniper/vmx-21.2R3.11.tgz", "vmx", 4096)
+_add("Juniper", "vMX 22.3R2.9", "vmx-22.3R2.9.tgz",
+     f"{_LH}/Juniper/vmx-22.3R2.9.tgz", "vmx", 4096)
+
 VENDORS = ["All vendors"] + sorted({e["vendor"] for e in CATALOG})
 
 
